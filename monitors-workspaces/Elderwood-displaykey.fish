1edb6c0323320a1c87e011d440e03aa9c20fc1aa
Sinh Nguyen <sinh@sinhn.com>
2024-08-05 16:49:56 +0700

    pomodoro & viber start

--- monitors-workspaces/Elderwood-displaykey.fish
+++ monitors-workspaces/Elderwood-displaykey.fish
@@ -1,4 +1,4 @@
 #!/usr/bin/env fish
 
-screenkey -p fixed -g 800x40+1000+1300 -f RobotoMonoNerdFontPropo --font-color "#e67285" &
-screenkey -p fixed -g 800x40+4200+1960 -f RobotoMonoNerdFontPropo --font-color "#e67285" &
+screenkey -p fixed -g 500x40+100+1350 -f RobotoMonoNerdFontPropo --font-color "#e67285" &
+screenkey -p fixed -g 500x40+5800+2050 -f RobotoMonoNerdFontPropo --font-color "#e67285" &
